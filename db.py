1e80c20f7ed7e7f54fc417807f75027d18cf18ab
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-08-23 00:52:29 +0530

    Added a file to generate the db and the required table.

---
+++ db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn  = sqlite3.connect('passwords.db')
+conn.execute("""CREATE TABLE IF NOT EXISTS users(
+        "user_name"    TEXT NOT NULL,
+        "master_password"    TEXT NOT NULL,
+        "passwords" TEXT[]
+        PRIMARY KEY("user_name")
+)""")
