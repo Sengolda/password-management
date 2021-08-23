cee28b209750a8283b0ff98cf06087edf4fa5533
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-08-23 17:42:08 +0530

    fixed the bug.

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ conn  = sqlite3.connect('passwords.db')
 conn.execute("""CREATE TABLE IF NOT EXISTS users(
         "user_name"    TEXT NOT NULL,
         "master_password"    TEXT NOT NULL,
-        "passwords" TEXT[]
+        "passwords" TEXT[],
         PRIMARY KEY("user_name")
 )""")
